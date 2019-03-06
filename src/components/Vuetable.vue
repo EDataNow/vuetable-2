e06a25467aa2b607d8436bf212bfa868a6beb15d
Shawn Lee-Kwong <splitnova@gmail.com>
2019-03-06 11:04:46 -0500

    check

--- src/components/Vuetable.vue
+++ src/components/Vuetable.vue
@@ -59,7 +59,7 @@
             </template>
           </tr>
           <template v-for="(item, index) in item.data">
-            <tr @dblclick="onRowDoubleClicked(item, $event)" :item-index="index" @click="onRowClicked(item, $event)" :render="onRowChanged(item)" :class="onRowClass(item, index), v-if="!item.hide || true">
+            <tr @dblclick="onRowDoubleClicked(item, $event)" :item-index="index" @click="onRowClicked(item, $event)" :render="onRowChanged(item)" :class="onRowClass(item, index), v-if="(item.hide === undefined ? true : !item.hide)">
               <template v-for="field in tableFields">
                 <template v-if="field.visible">
                   <template v-if="isSpecialField(field.name)">
